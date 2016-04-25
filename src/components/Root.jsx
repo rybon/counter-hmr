6539886137511ce6a13edab30b68ab5034315afa
Ryan Bonte <rbonte@libertyglobal.com>
2016-04-25 10:19:36 +0200

    only reload router when hmr enabled

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -3,12 +3,18 @@ import { Provider } from 'react-redux';
 import { Router } from 'react-router';
 import getRoutes from 'routes';
 
+let hmrKey = Math.random();
+
 const Root = ({ store, history }) => (
     <Provider store={store}>
-        <Router key={Math.random()} history={history}>
+        <Router key={hmrKey} history={history}>
             {getRoutes()}
         </Router>
     </Provider>
 );
+
+if (module.hot) {
+    hmrKey = Math.random();
+}
 
 export default Root;
