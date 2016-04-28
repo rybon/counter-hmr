e651ce25b3a307f13ca53c977f9e8709ba873407
Ryan Bonte <ryan.bonte@gmail.com>
2016-04-28 20:51:56 +0200

    updated deps, removed Math.random hack

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -3,18 +3,12 @@ import { Provider } from 'react-redux';
 import { Router } from 'react-router';
 import getRoutes from 'routes';
 
-let hmrKey = Math.random();
-
 const Root = ({ store, history }) => (
     <Provider store={store}>
-        <Router key={hmrKey} history={history}>
+        <Router history={history}>
             {getRoutes()}
         </Router>
     </Provider>
 );
-
-if (module.hot) {
-    hmrKey = Math.random();
-}
 
 export default Root;
